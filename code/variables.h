fa36d16e98f020d8c96196af043e7403a6b7f48c
Overlord-231 <milan.nagy@student.nhlstenden.com>
2024-02-27 10:22:47 +0100

    created pin variables

--- code/variables.h
+++ code/variables.h
@@ -1,6 +1,19 @@
 #ifndef VARIABLES_H
 #define VARIABLES_H
 
+//pins
+int buttonOnePin = 2;
+int neoPixelPin = 3;
+int RWFPin = 5; //right wheel forward
+int LWFPin = 6; //left wheel forward
+int triggerPin = 8;
+int echoPin = 9;
+int TLPin = 10; //turn left pin
+int TRPin = 11; //turn right pin
+int lineSensorPins[] = {12, 13, 14, 15, 16, 17, 18, 19}; 
+
+
+
 
 
 #endif
