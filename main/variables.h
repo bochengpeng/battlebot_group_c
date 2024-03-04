600fd59df87981df2f33550ee28853e8321125ce
Overlord-231 <milan.nagy@student.nhlstenden.com>
2024-03-04 12:01:39 +0100

    changed folder name

--- main/variables.h
+++ main/variables.h
@@ -1,12 +1,12 @@
 //pins
 const int buttonOnePin = 2;
 const int neoPixelPin = 3;
-const int rightForwardPin = 5; //right wheel forward
-const int leftForwardPin = 6; //left wheel forward
+const int rightForwardPin = 5; 
+const int leftForwardPin = 6; 
 const int triggerPin = 8;
 const int echoPin = 9;
-const int turnLeftPin = 10; //turn left pin
-const int turnRightPin = 11; //turn right pin
+const int turnLeftPin = 10; 
+const int turnRightPin = 11;
 const int lineSensorPins[] = {12, 13, 14, 15, 16, 17, 18, 19}; 
 bool hasItStarted = false;
 float sensorDistance = 0;
