db63f6251433edb4e78c549b67b4e473f3248e84
StefanCazacu1 <stefan.cazacu@student.nhlstenden.com>
2024-03-20 16:41:47 +0100

    update on whole movement logic and also servo start

--- movement/movement.ino
+++ movement/movement.ino
@@ -5,20 +5,26 @@ int R2 = 5;
 int counter = 0;
 int motorspeed1 = 234;
 int motorspeed2 = 255;
-int check = 0;
+int check = 1;
+int ultraServo = 11; // servo for the ultrasonic sensor
 
 int rightRotation = 2; //R1
 int leftRotation = 3; //R2
 volatile int rotationRight = 0;
 volatile int rotationLeft = 0;
 
-// Define pins for the ultrasonic sensor
-const int trigPin = 11; // Trigger pin
-const int echoPin = 12; // Echo pin
+// Define pins for front the ultrasonic sensor
+const int trigPinFront = 12; // Trigger pin Front
+const int echoPinFront = 13; // Echo pin Front
+const int trigPinSide = 7; // Trigger pin Side
+const int echoPinSide = 8; // Echo pin Side
 
 // Define variables for distance calculation
-long duration = 1000;
-int distance_cm = 100;
+long durationFront;
+long durationSide;
+
+int distanceCmFront;
+int distanceCmSide;
 
 void setup() {
 
@@ -28,18 +34,25 @@ void setup() {
   pinMode(R2 , OUTPUT);
 
   // Set trigPin as an output and echoPin as an input
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  pinMode(trigPinFront, OUTPUT);
+  pinMode(echoPinFront, INPUT);
+
+  pinMode(trigPinSide, OUTPUT);
+  pinMode(echoPinSide, INPUT);
 
   pinMode(rightRotation, INPUT);//rotation sensor
   pinMode(leftRotation, INPUT);//rotation sensor
 
   attachInterrupt(digitalPinToInterrupt(rightRotation),updateRightRotations,CHANGE);
   attachInterrupt(digitalPinToInterrupt(leftRotation) ,updateLeftRotations,CHANGE);
-  
+
+
+  pinMode(ultraServo , OUTPUT);
   Serial.begin(9600);
 
 }
+
+
 
 void stopRobot()
 {
@@ -64,15 +77,34 @@ void updateLeftRotations(){
 }
 
 void moveForwardInRotations (int rotations){
-  
-  if(rotationRight < rotations && rotationLeft < rotations){
-    analogWrite(L2,motorspeed1);
-    analogWrite(R1,motorspeed2);
-    delay(10);
+
+  if(distanceCmSide > 8)
+  {
+    analogWrite(L1, 0);
+    analogWrite(L2, motorspeed1);
+    analogWrite(R1, 180);
+    analogWrite(R2, 0);
+  }
+  else if(distanceCmSide < 7)
+  {
+    analogWrite(L1, 0);
+    analogWrite(L2, 180);
+    analogWrite(R1, motorspeed2);
+    analogWrite(R2, 0);
+  }
+  else if(rotationRight < rotations && rotationLeft < rotations){
+  // Motor 1
+  analogWrite(L1, 0);
+  analogWrite(L2, motorspeed1);
+  // Motor 2
+  analogWrite(R1, motorspeed2);
+  analogWrite(R2, 0);
+  delay(10);
   }
   else
   {
     resetRotations();
+    stopRobot();
   }
 }
  
@@ -115,45 +147,84 @@ void turnRight(int motorspeed1 , int motorspeed2) {
   // Motor 2
   analogWrite(R1, 0);
   analogWrite(R2, motorspeed2);
+  
 }
 
-void loop() {
-  // Clear the trigger pin before sending the signal
+int getUltrasonicCm(int trigPin , int echoPin)
+{
+  long duration;
+  int distance;
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
 
-  // Send a 10 microsecond pulse to the trigger pin
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
-  // Measure the duration of the pulse on the echo pin
   duration = pulseIn(echoPin, HIGH);
+  distance = (duration * 0.034) / 2;
 
-  // Calculate distance in centimeters
-  distance_cm = duration * 0.034 / 2;
-  //initialize the distance_cm variable when u start the robot to not make it turn right instatly
-  if(check == 0)
+  return distance;
+}
+
+void rotateUltraServoLeft()
+{
+  for(int i = 0 ; i<=5 ; i++)
   {
-    distance_cm = 11;
-    check = 1;
+    digitalWrite(ultraServo , HIGH);
+    delayMicroseconds(2400);
+    digitalWrite(ultraServo , LOW);
+    delayMicroseconds(17600);
   }
-  
-  // Print the distance to the serial monitor
-  Serial.print("Distance: ");
-  Serial.print(distance_cm);
-  Serial.println(" cm");
+}
 
-  // Check if the object is within 10cm
-  if (distance_cm <= 10) {
-    Serial.println("Object detected at 5cm or less!");
-    // Turn right
+void rotateUltraServo(int angle)
+{
+  for(int i = 0 ;  i<=10 ; i++)
+  {
+    int pulseWidth = map(angle, 90 , -90 , 600 , 2400);
+    digitalWrite(ultraServo , HIGH);
+    delayMicroseconds(pulseWidth);
+    digitalWrite(ultraServo , LOW);
+    delay(20);
+  }
+}
+
+int getDistanceLeft()
+{
+  stopRobot();
+  rotateUltraServoLeft();
+  return getUltrasonicCm(trigPinFront , echoPinFront);
+}
+
+void loop() {
+  rotateUltraServo(-90);
+  rotateUltraServo(0);
+  
+  distanceCmFront = getUltrasonicCm(trigPinFront , echoPinFront);
+  distanceCmSide = getUltrasonicCm(trigPinSide , echoPinSide);
+  
+  Serial.println(distanceCmFront);
+  if(distanceCmSide > 30)
+  {
+    moveForward(motorspeed1 , motorspeed2);
+    delay(450);
     turnRight(motorspeed1 , motorspeed2);
     delay(450);
-    
-  } else {
-    // Move forward if no obstacle detected
-      resetRotations();
-      moveForwardInRotations(20);
+    moveForward(motorspeed1 , motorspeed2);
+    delay(350);
   }
+  else if(distanceCmFront < 11 && distanceCmSide < 30)
+  {
+    turnLeft(motorspeed1 , motorspeed2);
+    delay(450);
+    moveForward(motorspeed1 , motorspeed2);
+    delay(350);
+  }
+  else 
+  {
+    Serial.println("Stuck in forward");
+    moveForwardInRotations(20);
+  }
+
 }
