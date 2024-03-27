86c33e4312b74e0ebafa3920623a7dc2aeddf225
bochengpeng <bocheng.peng@student.nhlstenden.com>
2024-03-27 11:25:25 +0100

    LED + Gripper Test

--- NeoPixelsSetting/NeoPixelsSetting.ino
+++ NeoPixelsSetting/NeoPixelsSetting.ino
@@ -11,9 +11,42 @@ Adafruit_NeoPixel pixels(NUM_PIXELS, PIN, NEO_GRB + NEO_KHZ800);
 #define COLOR_YELLOW  pixels.Color(255, 255, 0)
 #define COLOR_BLUE   pixels.Color(0, 0, 255)
 #define COLOR_PURPLE pixels.Color(255, 0, 255)
+#define COLOR_RED pixels.Color(0, 255, 0)
+
+#define gripperPin 10 // Pin connected to the servo signal wire
+// Define servo pulse width constants
+#define minPulseWidth 910 // Minimum pulse width in microseconds
+#define maxPulseWidth 2000 // Maximum pulse width in microseconds
+
+// Function to set servo angle 
+void setServoAngle(int angle, int servoPin)
+{
+  int pulseWidth = map(angle, 0, 180, minPulseWidth, maxPulseWidth);
+  digitalWrite(servoPin, HIGH); // Start the pulse
+  delayMicroseconds(pulseWidth); // Wait for the pulse width
+  digitalWrite(servoPin, LOW); // End the pulse
+  delay(20); // wait for servo to settle
+}
+
+void gripOpen()
+{
+  setServoAngle(90, gripperPin);
+}
+
+void gripClose()
+{
+  setServoAngle(0, gripperPin);
+}
+
+void grabCone() {
+  gripOpen(); // Open the gripper
+  delay(1000); // Wait for a duration to simulate grabbing the cone
+  gripClose(); // Close the gripper
+}
 
 void setup() {
   pixels.begin(); // Initialize NeoPixel strip
+  pinMode(gripperPin, OUTPUT);
 }
 
 void setColor(uint32_t color) {
@@ -33,4 +66,8 @@ void loop() {
   delay(1000); 
   setColor(COLOR_TEAL);
   delay(1000); 
+  setColor(COLOR_RED);
+  delay(1000); 
+   grabCone();
+  delay(2000);
 }
