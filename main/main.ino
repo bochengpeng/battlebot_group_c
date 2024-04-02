92bd5d6c3d253bce532fae3325341da989360d1e
NagyMilan111 <milan.nagy@student.nhlstenden.com>
2024-04-02 12:27:00 +0200

    arduino is terrible

--- main/main.ino
+++ main/main.ino
@@ -21,7 +21,7 @@ unsigned long lineTimer = 0;
 unsigned long previousTime = 0;
 unsigned long endTimer = 0;
 unsigned long avoidTimer = 0;
-int interval = 75;
+int interval = 10;
 unsigned long neoPixelTimer = 0;
 
 //lineSensor
@@ -104,14 +104,14 @@ void getLineSensorValues()
 		for(int j = 0; j < sizeof(lineSensorPins); j++)
     {
 			lineSensorValues[j] = analogRead(lineSensorPins[j]);
+      
 		}
-		lineTimer = 50 + millis();
+		lineTimer = 5 + millis();
  	}
 }
 
 void mainControl(){
 
-  getLineSensorValues();
   if(lineSensorValues[3] > 900){goForward();}
 	else if(lineSensorValues[4] > 900){goForward();}
 	else if(lineSensorValues[0] > 900){turnLeft(90);}
@@ -147,6 +147,7 @@ void putDownObject()
 void goForward(){
   if(millis() - previousTime >= interval)
   {
+    goForwardNeoPixel();
     analogWrite(leftForwardPin, 224);
     analogWrite(rightForwardPin, 224);
     digitalWrite(turnLeftPin, 0);
@@ -212,7 +213,7 @@ void end()
 
   if(lineSensorValues[0] > 900 && lineSensorValues[3] > 900 && lineSensorValues[7] > 900)
   {
-    
+    getLineSensorValues();
     if(millis() - endTimer >= 300)
     {
       getLineSensorValues();
@@ -323,10 +324,10 @@ void goForwardNeoPixel()
 void endNeoPixel()
 {
 
-  LEDs.setPixelColor(0, 56, 250, 2);
-  LEDs.setPixelColor(1, 255, 3, 3);
-  LEDs.setPixelColor(2, 5, 247, 235);
-  LEDs.setPixelColor(3, 231, 247, 2);
+  LEDs.setPixelColor(0, 255, 3, 196);
+  LEDs.setPixelColor(1, 255, 3, 196);
+  LEDs.setPixelColor(2, 255, 3, 196);
+  LEDs.setPixelColor(3, 255, 3, 196);
   LEDs.show();
 
 }
