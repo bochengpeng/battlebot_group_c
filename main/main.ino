6200e94a4457c3693ec141e54e3f050c8ad8b5d5
NagyMilan111 <milan.nagy@student.nhlstenden.com>
2024-04-05 10:54:01 +0200

    added comments, adjusted some values

--- main/main.ino
+++ main/main.ino
@@ -23,8 +23,7 @@ unsigned long lineTimer = 0;
 unsigned long previousTime = 0;
 unsigned long endTimer = 0;
 unsigned long previousSonicTime = 0;
-int interval = 50;
-unsigned long neoPixelTimer = 0;
+int interval = 50; //how often the movement controls should be executed
 
 //lineSensor
 int lineSensorValues[8];
@@ -34,6 +33,7 @@ bool hasStarted = false;
 bool hasEnded = false;
 bool theEnd = false;
 
+//NeoPixel
 Adafruit_NeoPixel LEDs(4, neoPixelPin, NEO_RGB + NEO_KHZ800);
 
 
@@ -59,12 +59,12 @@ void setup(){
   pinMode(A5, INPUT);
   pinMode(A6, INPUT);
   pinMode(A7, INPUT);
-  Serial.begin(9600);
 
   //other
   pinMode(neoPixelPin, INPUT);
   pinMode(gripperPin, OUTPUT);
-  digitalWrite(gripperPin, LOW);
+
+  //inital setup
   putDownObject();
   LEDs.begin();
   beforeStartNeoPixel();
@@ -85,24 +85,31 @@ void loop(){
   
 }
 
-//TODO:
-//add comments
+/* 
+   _____                               
+  / ____|                              
+ | (___   ___ _ __  ___  ___  _ __ ___ 
+  \___ \ / _ \ '_ \/ __|/ _ \| '__/ __|
+  ____) |  __/ | | \__ \ (_) | |  \__ \
+ |_____/ \___|_| |_|___/\___/|_|  |___/
+
+*/ 
 
 
 void getSensorDistance()
 {
   if(hasEnded == false)
   {
-    if(millis() - previousSonicTime >= 250)
+    if(millis() - previousSonicTime >= 250) //runs every 250 milliseconds
     {
       sensorDistance = 0;
-      digitalWrite(triggerPin, LOW);
+      digitalWrite(triggerPin, LOW); //make sure the trigger is turned off
       delayMicroseconds(3);
       digitalWrite(triggerPin, HIGH); //turn on trigger
       delayMicroseconds(9);
       digitalWrite(triggerPin, LOW); //turn off trigger
       sensorTime = pulseIn(echoPin, HIGH);  //get time
-      sensorDistance = (sensorTime*.0343)/2; //calculate distance
+      sensorDistance = (sensorTime*.0343)/2; //calculate distance, 0.343 is the speed of sound, which is divided by 2
       Serial.println(sensorDistance);
       previousSonicTime = millis();
     }
@@ -112,7 +119,7 @@ void getSensorDistance()
 void getLineSensorValues()
 {
 
-	if(millis() - lineTimer >= 40)
+	if(millis() - lineTimer >= 40) //runs every 40 milliseconds
   {
 		for(int j = 0; j < 8; j++)
     {
@@ -122,7 +129,52 @@ void getLineSensorValues()
 	}
 }
 
+/*
+   _____      _                       
+  / ____|    (_)                      
+ | |  __ _ __ _ _ __  _ __   ___ _ __ 
+ | | |_ | '__| | '_ \| '_ \ / _ \ '__|
+ | |__| | |  | | |_) | |_) |  __/ |   
+  \_____|_|  |_| .__/| .__/ \___|_|   
+               | |   | |              
+               |_|   |_|              
+*/
+
+
+void pickUpObject()
+{
+  for(int i = 0; i < 15; i++) //we have to execute this 15 times to guarantee reliability
+  {
+    digitalWrite(gripperPin, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(gripperPin, LOW);
+  }
+}
+
+void putDownObject()
+{
+  for(int i = 0; i < 15; i++) //same here
+  {
+   digitalWrite(gripperPin, HIGH);
+   delayMicroseconds(1700);
+   digitalWrite(gripperPin, LOW);
+  }
+}
+
+/*
+  __  __       _   _                               _             _     
+ |  \/  |     | | (_)                             | |           | |    
+ | \  / | ___ | |_ _  ___  _ __     ___ ___  _ __ | |_ _ __ ___ | |___ 
+ | |\/| |/ _ \| __| |/ _ \| '_ \   / __/ _ \| '_ \| __| '__/ _ \| / __|
+ | |  | | (_) | |_| | (_) | | | | | (_| (_) | | | | |_| | | (_) | \__ \
+ |_|  |_|\___/ \__|_|\___/|_| |_|  \___\___/|_| |_|\__|_|  \___/|_|___/
+                                                                                                                                              
+*/
+
+
 void mainControl(){
+
+  //main control logic based on the lineSensorValues
 
   if(lineSensorValues[3] > 900){goForward();}
 	else if(lineSensorValues[4] > 900){goForward();}
@@ -133,27 +185,6 @@ void mainControl(){
 	else if(lineSensorValues[6] > 900){turnRight(40);}
 	else if(lineSensorValues[7] > 900){turnRight(50);}
   
-}
-
-
-void pickUpObject()
-{
-  for(int i = 0; i < 15; i++)
-  {
-    digitalWrite(gripperPin, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(gripperPin, LOW);
-  }
-}
-
-void putDownObject()
-{
-  for(int i = 0; i < 15; i++)
-  {
-   digitalWrite(gripperPin, HIGH);
-   delayMicroseconds(1700);
-   digitalWrite(gripperPin, LOW);
-  }
 }
 
 void goForward(){
@@ -214,65 +245,6 @@ void reverse()
 	analogWrite(turnRightPin, 0);
 }
 
-void start()
-{
-
-  if(hasEnded == false && sensorDistance > 10 && hasStarted == false)
-  {
-    startNeoPixel();
-    hasStarted = true;
-    goForward();
-    delay(1000);
-    pickUpObject();
-    turnLeft(255);
-    delay(700);
-    goForward();
-  } 
-}
-
-void end()
-{
-
-  if(lineSensorValues[0] > 900 &&
-     lineSensorValues[1] > 900 && 
-     lineSensorValues[2] > 900 &&
-     lineSensorValues[3] > 900 &&
-     lineSensorValues[4] > 900 &&
-     lineSensorValues[5] > 900 &&
-     lineSensorValues[6] > 900 &&
-     lineSensorValues[7] > 900 && hasEnded == false)
-  {
-    getLineSensorValues();
-    if(millis() - endTimer >= 150)
-    {
-      if(lineSensorValues[0] > 900 &&
-         lineSensorValues[1] > 900 && 
-         lineSensorValues[2] > 900 &&
-         lineSensorValues[3] > 900 &&
-         lineSensorValues[4] > 900 &&
-         lineSensorValues[5] > 900 &&
-         lineSensorValues[6] > 900 &&
-         lineSensorValues[7] > 900 && theEnd == true)
-      {
-        endNeoPixel();
-        hasEnded = true;
-        hasStarted = false;
-        stop();
-        putDownObject();
-        delay(500);
-        reverse();
-        delay(1000);
-        stop();
-      }
-      else{theEnd = false;}
-      endTimer = millis();
-    }
-    theEnd = true;
-  }
-  else{theEnd = false;}
-
-}
-
 void avoidObject()
 {
   getSensorDistance();
@@ -306,6 +278,99 @@ void avoidObject()
 
   }
 }
+
+/*
+   _____ _             _                     _   ______           _ 
+  / ____| |           | |                   | | |  ____|         | |
+ | (___ | |_ __ _ _ __| |_    __ _ _ __   __| | | |__   _ __   __| |
+  \___ \| __/ _` | '__| __|  / _` | '_ \ / _` | |  __| | '_ \ / _` |
+  ____) | || (_| | |  | |_  | (_| | | | | (_| | | |____| | | | (_| |
+ |_____/ \__\__,_|_|   \__|  \__,_|_| |_|\__,_| |______|_| |_|\__,_|
+                                                                                                                                        
+*/
+
+void start()
+{
+
+  if(hasEnded == false && sensorDistance > 20 && hasStarted == false)
+  {
+    //inital starting sequence, we can take manual control of the bot here
+    startNeoPixel();
+    hasStarted = true;
+    goForward();
+    delay(1000);
+    pickUpObject();
+    turnLeft(255);
+    delay(700);
+    goForward();
+  } 
+}
+
+void end()
+{
+
+  /* the main logic here is that if we see a potential black box we set
+     theEnd to true, get the line sensor values, BUT we don't end the race just yet,
+     after 200 milliseconds we check if the potential black box is still there, if not then set
+     theEnd to be false, however if it is still there then we start the ending sequence
+  */
+
+  if(lineSensorValues[0] > 900 &&
+     lineSensorValues[1] > 900 && 
+     lineSensorValues[2] > 900 &&
+     lineSensorValues[3] > 900 &&
+     lineSensorValues[4] > 900 &&
+     lineSensorValues[5] > 900 &&
+     lineSensorValues[6] > 900 &&
+     lineSensorValues[7] > 900 && 
+     hasEnded == false)
+  {
+    getLineSensorValues();
+    if(millis() - endTimer >= 200)
+    {
+      if(lineSensorValues[0] > 900 &&
+         lineSensorValues[1] > 900 && 
+         lineSensorValues[2] > 900 &&
+         lineSensorValues[3] > 900 &&
+         lineSensorValues[4] > 900 &&
+         lineSensorValues[5] > 900 &&
+         lineSensorValues[6] > 900 &&
+         lineSensorValues[7] > 900 && 
+         theEnd == true)
+      {
+        endNeoPixel();
+        hasEnded = true;
+        hasStarted = false;
+        stop();
+        putDownObject();
+        delay(500);
+        reverse();
+        delay(1000);
+        stop();
+      }
+      else
+      {
+        theEnd = false;
+      }
+      endTimer = millis();
+    }
+    theEnd = true;
+  }
+  else
+  {
+    theEnd = false;
+  }
+}
+
+/*
+  _   _            _____ _          _     
+ | \ | |          |  __ (_)        | |    
+ |  \| | ___  ___ | |__) |__  _____| |___ 
+ | . ` |/ _ \/ _ \|  ___/ \ \/ / _ \ / __|
+ | |\  |  __/ (_) | |   | |>  <  __/ \__ \
+ |_| \_|\___|\___/|_|   |_/_/\_\___|_|___/
+                                                                                    
+*/
 
 void beforeStartNeoPixel()
 {
